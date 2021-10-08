fb8ab50841cdcbe787da762c83b7ff4dc799bd26
ErkinKurt <kurt.erkin@gmail.com>
2021-10-08 15:28:26 +0300

    Rename number one to number

--- lib/rules/bigger_than_rule.dart
+++ lib/rules/bigger_than_rule.dart
@@ -1,14 +1,14 @@
 import 'package:fluent_validator/core/rule.dart';
 
 class BiggerThanRule extends Rule {
-  BiggerThanRule(this.numberOne, String errorMessage) : super(errorMessage);
+  BiggerThanRule(this.number, String errorMessage) : super(errorMessage);
 
-  final num numberOne;
+  final num number;
 
   @override
   bool isValid(dynamic value) {
     if (value is num) {
-      return value > numberOne;
+      return value > number;
     }
     throw UnimplementedError();
   }
